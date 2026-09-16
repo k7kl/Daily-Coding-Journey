79ce60e7773a32cf6c8b4936bcc95e983919fc50
Abdulrazaq <ddabo6807@gmail.com>
2026-09-16 17:54:22 +0300

    Add problem 24: Lower First Letter of Each Word

---
+++ Course_7/Problem_24/Problem_24.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadString() {
+	string Word;
+	cout << "Please enter anything: ";
+	getline(cin, Word);
+	return Word;
+}
+
+string LowerFirstLetterOfEachWord(string Word) {
+	bool IsFirstLetter = true;
+
+	for (int i = 0; i < Word.length(); i++) {
+		if (Word[i] != ' ' && IsFirstLetter) {
+			Word[i] = tolower(Word[i]);
+			IsFirstLetter = false;
+		}
+
+		if (Word[i] == ' ') {
+			IsFirstLetter = true;
+		}
+	}
+	return Word;
+}
+
+int main()
+{
+	string Word = ReadString();
+	cout << "\nString after conversion:\n";
+	cout << LowerFirstLetterOfEachWord(Word);
+}
